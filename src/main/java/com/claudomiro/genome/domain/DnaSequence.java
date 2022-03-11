b3c3ebaeaba6973ac61866a90c87d25fbf2c885e
Claudomiro Junior <claudomiro>
2022-03-11 07:39:33 -0300

    Extraccted superclass

---
+++ src/main/java/com/claudomiro/genome/domain/DnaSequence.java
@@ -0,0 +1,47 @@
+package com.claudomiro.genome.domain;
+
+import com.claudomiro.genome.dto.SimianRequestDTO;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class DnaSequence {
+    protected List<String> dna = new ArrayList<>();
+
+    public static SimianRequestDTO fromStringArray(String[] dna) {
+
+        final List<String> asList = Arrays.asList(dna);
+        final SimianRequestDTO o = new SimianRequestDTO();
+        o.setDna(Arrays.asList(dna));
+        return o;
+    }
+
+    public List<String> getDna() {
+        return dna;
+    }
+
+    public void setDna(List<String> dna) {
+        if (dna == null) {
+            throw new NullPointerException("String list could not be null");
+        }
+        final int expectedLength = dna.get(0).length();
+        for (String str : dna) {
+            if (str.length() != expectedLength) {
+                throw new IllegalArgumentException("Expecting matrix " +
+                        expectedLength + "x" + expectedLength);
+            }
+        }
+
+        for (String str : dna) {
+            for (int i = 0; i < str.length(); i++) {
+                char c = str.charAt(i);
+                final String validChars = "ACGT";
+                if (!validChars.contains(str.substring(i, i + 1))) {
+                    throw new IllegalArgumentException("I dont know how to process char '" + c + "' (Must be one of ACGT");
+                }
+            }
+        }
+        this.dna = dna;
+    }
+}
--- src/main/java/com/claudomiro/genome/dto/SimianRequestDTO.java
+++ src/main/java/com/claudomiro/genome/dto/SimianRequestDTO.java
@@ -4,45 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class SimianRequestDTO {
-
-    public static SimianRequestDTO fromStringArray(String[] dna) {
-
-        final List<String> asList = Arrays.asList(dna);
-        final SimianRequestDTO o = new SimianRequestDTO();
-        o.setDna(Arrays.asList(dna));
-        return o;
-    }
-
-    private List<String> dna = new ArrayList<>();
-
-    public List<String> getDna() {
-        return dna;
-    }
-
-    public void setDna(List<String> dna) {
-        if(dna == null) {
-            throw new NullPointerException("String list could not be null");
-        }
-        final int expectedLength = dna.get(0).length();
-        for (String str : dna) {
-            if(str.length() != expectedLength) {
-                throw new IllegalArgumentException("Expecting matrix " +
-                        expectedLength + "x" + expectedLength);
-            }
-        }
-
-        for (String str : dna) {
-            for (int i = 0 ; i < str.length(); i++) {
-                char c = str.charAt(i);
-                final String validChars = "ACGT";
-                if(!validChars.contains(str.substring(i, i+1))) {
-                    throw new IllegalArgumentException("I dont know how to process char '" + c + "' (Must be one of ACGT");
-                }
-            }
-        }
-        this.dna = dna;
-    }
+public class SimianRequestDTO extends com.claudomiro.genome.domain.DnaSequence {
 
     @Override
     public String toString() {
